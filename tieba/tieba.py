0cad982feb20b7ad3b38cd43892ba92cfc699849
fcfangcc <swjfc22@163.com>
2015-11-23 21:29:06 +0800

    添加获取贴吧,部分函数

--- tieba/tieba.py
+++ tieba/tieba.py
@@ -8,7 +8,8 @@ import requests
 import sys
 import os
 import shutil
-import time
+import json
+from collections import defaultdict
 
 HEADERS = {'content-type': 'application/json',
            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
@@ -25,8 +26,14 @@ def user_main(name):
 class InputErrorStr(Exception):
     pass
 
+
 class TieziError(Exception):
     pass
+
+
+class TiebaError(Exception):
+    pass
+
 
 class UserError(Exception):
     pass
@@ -47,6 +54,10 @@ class User(object):
             sys.exit()
 
     def get_followBa(self):
+        """
+        返回用户关注的贴吧
+        :return:
+        """
         followBa = self.userHtml.findall('.//*[@id="forum_group_wrap"]/a/span')
         j = 0
         dictionary = {}
@@ -69,10 +80,18 @@ class User(object):
         return num.text
 
     def get_age(self):
+        """
+        获取用户吧龄
+        :return:
+        """
         age = self.userHtml.find('.//*[@id="userinfo_wrap"]/div[2]/div[3]/div/span[2]')
         return age.text
 
     def get_sex(self):
+        """
+        返回用户性别
+        :return:
+        """
         sex = self.userHtml.find('.//*[@id="userinfo_wrap"]/div[2]/div[3]/div/span[1]')
         return sex.attrib['class']
 
@@ -153,6 +172,11 @@ class User(object):
                 raise InputErrorStr, 'STR ERROR ,it can only input num,all,users!'
 
     def get_recently_tie(self, num=10):
+        """
+        返回用户最近的回帖信息
+        :param num:
+        :return:
+        """
         if self.userHtml.find('.//*[@id="container"]/div[1]/div/div[3]').text:
             return u"该用户已隐藏个人动态"
         times = self.userHtml.findall('.//*[@id="container"]/div[1]/div/div[3]/ul/div/div[1]')
@@ -202,6 +226,12 @@ class Tiezi(object):
                 print u"输入的参数有误，只能输入'txt'或者'photo'"
 
     def __get_lz_txt(self, page, soup):
+        """
+        下载帖子的只看楼主的所有楼主文字内容
+        :param page:
+        :param soup:
+        :return:
+        """
         print u'正在下载第 %d 页内容，请等待......\n' % page
         txts = soup.xpath('.//div[@class="d_post_content j_d_post_content "]/text()')
         if not txts:
@@ -211,6 +241,12 @@ class Tiezi(object):
         f.close()
 
     def __get_lz_jpg(self, page, soup):
+        """
+        下载帖子的只看楼主的所有的发布图片
+        :param page:
+        :param soup:
+        :return:
+        """
         photos = soup.xpath('//img[@class="BDE_Image"]')
         if len(photos) == 0:
             print u"第%d页没有检测到有楼主发布的图片" % page
@@ -234,6 +270,12 @@ class Tiezi(object):
         os.makedirs(path)
 
     def __download_jpg(self, src, path):
+        """
+        图片下载函数
+        :param src:
+        :param path:
+        :return:
+        """
         try:
             urllib.urlretrieve(src, path)
         except Exception as e:
@@ -241,14 +283,90 @@ class Tiezi(object):
             print u"下载图片失败!"
 
     def getnum(self):
+        """
+        获取楼主发表的页面数
+        :return:
+        """
         num = self.soup.xpath('.//*[@id="thread_theme_5"]/div[1]/ul/li[2]/span[2]')
         return num[0].text
 
     def _is404(self):
+        """
+        检查帖子是否存在或者删除
+        :return:
+        """
         if self.soup.xpath('.//*[@class="page404"]'):
             return True
         else:
             return False
+
+
+class Tieba(object):
+    def __init__(self, name):
+        # self.name = repr(name.decode('UTF-8').encode('gbk')).replace("\'", "").replace("\\x", "%")
+        self.name = name
+        url_items = [MAIN_URL, "f?kw=", name, "&fr=index"]
+        self.url = ''.join(url_items)
+        self.html = requests.get(self.url, headers=HEADERS).content
+        self.soup = soupparser.fromstring(self.html)
+        if not self.exist():
+            raise TiebaError,'The tieba: "%s" have not exist!' % self.name
+
+    def get_name(self):
+        name = soup.xpath('.//*[@class="card_title_fname"]')
+        return name[0].text
+
+    def get_follow(self):
+        num = self.soup.xpath('.//*[@class="card_menNum"]')
+        return num[0].text
+
+    def get_tienum(self):
+        num = self.soup.xpath('.//*[@class="card_infoNum"]')
+        return num[0].text
+
+    def get_catalog(self):
+        """
+        获取贴吧标签目录
+        :return:list
+        """
+        catalogs = soup.xpath('.//*[@class="card_info"]/ul/li/a')
+        return [i.text for i in catalogs]
+
+    def get_ties(self):
+        x = soup.xpath('.//*[@id="thread_list"]/li')
+        for i in x:
+            try:
+                print json.loads(i.attrib['data-field'])['id']
+            except:
+                pass
+
+    def sign(self):
+        pass
+
+    def follow(self):
+        pass
+
+    def un_follow(self):
+        pass
+
+    def create_tie(self, title, content):
+        pass
+
+    def exist(self):
+        if self.soup.xpath('.//*[@class="sign_today_date"]'):
+            return True
+        else:
+            return False
+
+    def save_html(self, path=''):
+        path = "%s.html" % self.name if path else path+"%s.html" % self.name
+        with open(path, 'w') as f:
+            f.write(self.html)
+        print "html save succeed!"
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -256,5 +374,20 @@ if __name__ == '__main__':
     # n = ["4079508183", "4045828597", "3926556029"]
     # c = Tiezi(n[0])
     # print c.getnum()
-    pass
+    # pass
+
+    # with open("zjgsdx.html","r") as f:
+    #     text = f.read()
+    # soup = soupparser.fromstring(text)
+    # x = soup.xpath('.//*[@id="thread_list"]/li')
+    # for i in x:
+    #     try:
+    #         d = json.loads(i.attrib['data-field'])
+    #         del d['vid']
+    #         del d['is_good']
+    #         del d['is_top']
+    #         del d['is_protal']
+    #         print d['is_bakan']
+    #     except:
+            pass
 
